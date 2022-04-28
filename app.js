4385201ee7518a2358562e529a3b84a2a48bb98d
Devraj <195040@nith.ac.in>
2022-04-28 13:03:51 +0530

    database craeted

--- app.js
+++ app.js
@@ -5,16 +5,39 @@ const app=express();
 app.set("view engine","ejs");
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
-var items=[];
-var temp="example";
+const mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost:27017/todo");
+const tryScehema=new mongoose.Schema({
+    name: String
+});
+const item=mongoose.model("task",tryScehema);
+
 app.get("/",(req,res)=>{
-    res.render("list",{ejes:items});
-})
+      item.find({},(err,foundItems)=>{
+        if(err){
+            console.log(err);
+        }  
+        else{
+            res.render("list",{ejes:foundItems});
+        }
+      }) ;
+});
 app.post("/",(req,res)=>{
-var item=(req.body.ele1);
-items.push(item);
+var itemName=(req.body.ele1);
+const todo2=new item({
+    name :itemName
+});
+todo2.save();
 res.redirect("/");
 });
+app.post("/delete",(req,res)=>{
+    const checked=req.body.checkbox1;
+    item.findByIdAndRemove(checked,(err)=>{
+        if(!err)
+       {res.redirect("/") }
+    });
+});
+
 
 
 
